252ef6c01384ebbdbb5479555c2e978aa7be3d11
igor_grigoriev <stellardev8@gmail.com>
2022-01-11 19:22:47 +0300

    аутентификация и авторизация

--- src/main/java/ru/master/api/controller/RunController.java
+++ src/main/java/ru/master/api/controller/RunController.java
@@ -9,7 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RunController {
   @GetMapping("/")
-  public ResponseEntity<?> runOne() {
+  public ResponseEntity<?> home() {
     return ResponseEntity.ok("<h1>Welcome</h1>");
+  }
+
+  @GetMapping("/user")
+  public ResponseEntity<?> user() {
+    return ResponseEntity.ok("<h1>Welcome User</h1>");
+  }
+
+  @GetMapping("/admin")
+  public ResponseEntity<?> admin() {
+    return ResponseEntity.ok("<h1>Welcome Admin</h1>");
   }
 }
